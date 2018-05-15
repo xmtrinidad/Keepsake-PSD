91c0e1cfa28c77e028083e26b1859c652d2bbe02
Xavier Trinidad <xmtrinidad@gmail.com>
2018-05-14 22:02:35 -0700

    Added footer styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   mobileNavActive = false;
   navItems = ['HOME', 'ABOUT US', 'OUR WORKS', 'JOURNAL', 'CONTACT'];
+  socialIcons: {icon: string, color: string}[] = [
+    {
+      icon: 'fas fa-rss',
+      color: '#fbaa56',
+    },
+    {
+      icon: 'fab fa-facebook',
+      color: '#667eb7',
+    },
+    {
+      icon: 'fab fa-twitter',
+      color: '#6cc1eb',
+    },
+    {
+      icon: 'fab fa-pinterest',
+      color: '#da535b',
+    },
+  ];
 
   onMobileMenuClick() {
     this.mobileNavActive = this.mobileNavActive !== true;
